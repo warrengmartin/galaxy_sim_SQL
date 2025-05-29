53eccf8fa215693acab9c7bc50709952a73986fa
warrengmartin <warreng.martin@gmail.com>
2025-05-29 04:21:42 +0000

    fix: revert typeOfSimulation from 2 to 1 in effectController

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ effectController = {
     height: height,
     middleVelocity: middleVelocity,
     velocity: velocity,
-    typeOfSimulation: 2,
+    typeOfSimulation: 1,
     autoRotation: false
 };
 
@@ -124,7 +124,7 @@ function selectChoice(choice) {
             height: height,
             middleVelocity: middleVelocity,
             velocity: 7,
-            typeOfSimulation: 2,
+            typeOfSimulation: 1,
             autoRotation: false
         };
     }
